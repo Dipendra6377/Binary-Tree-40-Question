937e1654d427bfb3e42d25f646e420ec45434e9d
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-02-06 15:15:12 +0530

    Update 03_Preorder traversal.java

--- 06-February-2023/Question 1 to Question 10/03_Preorder traversal.java
+++ 06-February-2023/Question 1 to Question 10/03_Preorder traversal.java
@@ -1,3 +1,5 @@
+// RECURSIVE
+
 class BinaryTree
 {
     //Function to return a list containing the preorder traversal of the tree.
@@ -16,4 +18,39 @@ class BinaryTree
         }
     }
 
+}
+
+//ITERATIVE
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> q=new Stack<>();
+        List<Integer> list=new LinkedList<>();
+
+        if(root==null) return list;
+        q.push(root);
+        while(!q.isEmpty()){
+            TreeNode temp=q.pop();
+            list.add(temp.val);
+            if(temp.right!=null) q.push(temp.right);
+
+            if(temp.left!=null) q.push(temp.left);
+        }
+        return list;
+    }
 }
