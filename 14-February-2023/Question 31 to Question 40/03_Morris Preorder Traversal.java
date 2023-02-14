853f8a684dcf6555cd82ee6d30b45c2d65e90e39
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-02-14 12:28:52 +0530

    Create 03_Morris Preorder Traversal.java

---
+++ 14-February-2023/Question 31 to Question 40/03_Morris Preorder Traversal.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left==null){
+                list.add(curr.val);
+                curr=curr.right;
+            }
+            else{
+                TreeNode prev=curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev=prev.right;
+                }
+                if(prev.right==null){
+                    prev.right=curr;
+                    list.add(curr.val);
+                    curr=curr.left;
+                }
+                else{
+                    prev.right=null;
+                    curr=curr.right;
+                }
+            }
+        }
+        return list;
+    }
+}
